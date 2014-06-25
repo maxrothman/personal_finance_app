d5e98f34c381dbe6c3a712734462b35511e3df64
max.rothman <max.rothman@vecna.com>
2014-06-25 13:13:34 -0400

    Redid virtualenv to be relocatable, added flask-restful

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/maxrothman/personal_finance_app/venv"
+set -gx VIRTUAL_ENV "/other/personal_finance_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
